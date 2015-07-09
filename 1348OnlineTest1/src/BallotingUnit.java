211097c3267dca622ebe04a66a71f6787c59afc0
saield <saiel2474@gmail.com>
2015-07-09 15:36:36 +0530

    class ver 1

---
+++ 1348OnlineTest1/src/BallotingUnit.java
@@ -0,0 +1,19 @@
+
+public class BallotingUnit {
+	private Boolean status;
+	
+	public BallotingUnit(){
+		status = true;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+	
+	
+}
---
+++ 1348OnlineTest1/src/CandidateLamp.java
@@ -0,0 +1,4 @@
+
+public class CandidateLamp implements Lamp{
+
+}
